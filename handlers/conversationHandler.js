f5c6b6428b87a191f27e00dc8bb538228f5f1d3c
shoxie <sktt1lka@gmail.com>
2018-12-21 19:29:49 +0700

    Update conversationHandler.js

--- handlers/conversationHandler.js
+++ handlers/conversationHandler.js
@@ -4,11 +4,9 @@
  * @license MIT
  */
 
-const CLEVERBOT = require('cleverbot.js');
+
 const CREDENTIALS = require('../settings/credentials.json');
-const BOT = new CLEVERBOT({
-  APIKey: CREDENTIALS.cleverbotAPIkey
-});
+
 
 /**
  * Handles conversations with Bastion
@@ -17,24 +15,7 @@ const BOT = new CLEVERBOT({
  */
 module.exports = async message => {
   try {
-    if (message.content.length <= `${message.client.user}  `.length) return;
 
-    let guild = await message.client.db.get(`SELECT chat FROM guildSettings WHERE guildID=${message.guild.id}`);
-    if (!guild.chat) return;
-
-    let response = await BOT.write(message.content);
-    if (response.output) {
-      message.channel.startTyping();
-      setTimeout(async () => {
-        try {
-          message.channel.stopTyping(true);
-          await message.channel.send(response.output);
-        }
-        catch (e) {
-          message.client.log.error(e);
-        }
-      }, response.output.length * 100);
-    }
   }
   catch (e) {
     message.client.log.error(e);
